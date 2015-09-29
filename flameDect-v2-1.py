e1a8f9e351ce8fc9945516f7d410d56c4f8f23a1
stone-ch <great_cy_ang@163.com>
2015-09-29 23:19:18 +0800

    modify

--- flameDect-v2-1.py
+++ flameDect-v2-1.py
@@ -1,33 +1,33 @@
 #! /usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++
-#存在的问题：
-#1.RGB下火焰像素点的规律未掌握
-#2.程序现在的运行速度是每祯处理2秒
+# ++++++++++++++++++++++++++++++++++++++++++++++++++
+# 存在的问题：
+# 1.RGB下火焰像素点的规律未掌握
+# 2.程序现在的运行速度是每祯处理2秒
 #
-#先灰度化，二值化，中值滤波，开运算，
-#根据颜色找火焰区域，画轮廓
+# 先灰度化，二值化，中值滤波，开运算，
+# 根据颜色找火焰区域，画轮廓
 #
-#author：stone 2015.08.30
-#++++++++++++++++++++++++++++++++++++++++++++++++++
+# author：stone 2015.08.30
+# ++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import numpy as np
 import cv2
 import time
 
 tStart = time.time()
-#++++++++++++++++++++++获取视频+++++++++++++++++++++
-vName = '../video/forest1.avi'
-#Boat_Fire_Stream.wmv
-#controlled3.avi
-#Extreme_Fire_Scenes_Stream.wmv
-#forest1.avi
-#forest2.avi
+# ++++++++++++++++++++++获取视频+++++++++++++++++++++
+vName = 'videos/forest1.avi'
+# Boat_Fire_Stream.wmv
+# controlled3.avi
+# Extreme_Fire_Scenes_Stream.wmv
+# forest1.avi
+# forest2.avi
 
 cap = cv2.VideoCapture(vName)
 
-#+++++++++++++++++++++处理视频++++++++++++++++++++++
+# +++++++++++++++++++++处理视频++++++++++++++++++++++
 while True:
     ret, frame = cap.read()
     if frame is None:
@@ -40,26 +40,26 @@ while True:
     npframe = np.array(frame)
     # nphsv = np.array(hsv)
     npbinary = np.array(binary)
-#+++++++++++++++++++++去噪+++++++++++++++++++++++++
-#二维卷积
-#    kernel = np.ones((5, 5), np.float32)/25
-#    dst = cv2.filter2D(binary, -1, kernel)
+    # +++++++++++++++++++++去噪+++++++++++++++++++++++++
+    # 二维卷积
+    #    kernel = np.ones((5, 5), np.float32)/25
+    #    dst = cv2.filter2D(binary, -1, kernel)
 
-#中值模糊
+    # 中值模糊
     dst = cv2.medianBlur(binary, 5)
 
-#高斯模糊
-#    dst = cv2.GaussianBlur(binary, (5, 5), 0)
+    # 高斯模糊
+    #    dst = cv2.GaussianBlur(binary, (5, 5), 0)
 
-#均值模糊
-#    dst = cv2.blur(binary, (5, 5))
+    # 均值模糊
+    #    dst = cv2.blur(binary, (5, 5))
 
-#+++++++++++++++做开运算（先腐蚀再膨胀）除噪点++++++++++++
+    # +++++++++++++++做开运算（先腐蚀再膨胀）除噪点++++++++++++
     kernal2 = np.ones((5, 5), np.uint8)
     opening = cv2.morphologyEx(dst, cv2.MORPH_OPEN, kernal2)
     openning2 = cv2.morphologyEx(dst, cv2.MORPH_OPEN, kernal2)
 
-#+++++++++++++++判断亮点颜色是否R >= G > B+++++++++++++
+    # +++++++++++++++判断亮点颜色是否R >= G > B+++++++++++++
     binary_x = 0
     Rt = 135
     St = 55
@@ -69,47 +69,47 @@ while True:
             if (opening[binary_x, binary_y] == 255):
                 p = npframe[(binary_x, binary_y)]
                 # p2 = hsv[(binary_x, binary_y)]
-#    ？？？？？？怎么转换到hsv颜色空间，然后再增加一条限制： S >= （（255-R）*ST/RT）
-#？？？？？？加上S限制之后效果不好，可能是S有误，需要研究一下HSV 或者 HSI 或者其他有S的颜色空间
-                if p[2] >= p[1] >= p[0]:     # & p2[2] >= 5:
+                #    ？？？？？？怎么转换到hsv颜色空间，然后再增加一条限制： S >= （（255-R）*ST/RT）
+                # ？？？？？？加上S限制之后效果不好，可能是S有误，需要研究一下HSV 或者 HSI 或者其他有S的颜色空间
+                if p[2] >= p[1] >= p[0]:  # & p2[2] >= 5:
                     binary_y += 1
                     continue
                 else:
                     opening[binary_x, binary_y] = 0
             binary_y += 1
         binary_x += 1
-#+++++++++++++++显示高亮区域轮廓+++++++++++++++++++++++
+    # +++++++++++++++显示高亮区域轮廓+++++++++++++++++++++++
     image, contour, hierarchy = cv2.findContours(opening,
-        cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                                                 cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if contour == []:
         print('无轮廓')
     else:
-#    在视频窗口中显示轮廓
+        #    在视频窗口中显示轮廓
         cv2.drawContours(frame, contour, -1, (0, 0, 255), 2)
 
-#+++++++++++++++图像中的点是否在轮廓内++++++++++++++++++++
+        # +++++++++++++++图像中的点是否在轮廓内++++++++++++++++++++
 
-#？？？无轮廓时索引异常
-#    if np.array(contour)[0] == None:
-#        print '无轮廓'
-#    else:
-#        npcontour = np.array(contour)[0]
-#    npframe = np.array(frame)
-#    t = time.time()
-#    for x in xrange(height):
-#        for y in xrange(width):
-#            inContour = cv2.pointPolygonTest(npcontour, (x, y), False)
-#            if (inContour > 0):
-#                print npframe[(x, y)]
-#                t1 = time.time()
-#                print t - t1
+        # ？？？无轮廓时索引异常
+    #    if np.array(contour)[0] == None:
+    #        print '无轮廓'
+    #    else:
+    #        npcontour = np.array(contour)[0]
+    #    npframe = np.array(frame)
+    #    t = time.time()
+    #    for x in xrange(height):
+    #        for y in xrange(width):
+    #            inContour = cv2.pointPolygonTest(npcontour, (x, y), False)
+    #            if (inContour > 0):
+    #                print npframe[(x, y)]
+    #                t1 = time.time()
+    #                print t - t1
 
-#++++++++++++++++++++++显示视频++++++++++++++++++++++
-    cv2.imshow('frame', frame)      #原图像
+    # ++++++++++++++++++++++显示视频++++++++++++++++++++++
+    cv2.imshow('frame', frame)  # 原图像
     cv2.imshow('binary', binary)
     # cv2.imshow('hsv', hsv)
-#    cv2.imshow('contour', opening)  #开运算图像
-#    cv2.imshow('opening2', openning2)          #中值模糊图像
+    #    cv2.imshow('contour', opening)  #开运算图像
+    #    cv2.imshow('opening2', openning2)          #中值模糊图像
     tEnd = time.time()
     totalTime = (tEnd - tStart)
     print(tStart)
@@ -118,10 +118,10 @@ while True:
     if cv2.waitKey(1) & 0xFF == 27:
         print('中止播放')
         break
-#tEnd = time.time()
+# tEnd = time.time()
 
 cap.release()
 cv2.destroyAllWindows()
-#totalTime = (tEnd - tStart)
+# totalTime = (tEnd - tStart)
 print(time.clock())
-#print '总时间 %d ' % totalTime
+# print '总时间 %d ' % totalTime
--- smokeDect-v0.py
+++ smokeDect-v0.py
@@ -9,23 +9,39 @@ import cv2
 vName = 'videos/CTC_FG.028_9.mpg'
 # CarLights1.avi
 # CTC_FG.028_9.mpg
+# SmokeVideo.mp4
 
 cap = cv2.VideoCapture(vName)
 
-fgbg = cv2.createBackgroundSubtractorMOG2()
+fgbg = cv2.createBackgroundSubtractorKNN()
 
 while True:
     ret, frame = cap.read()
-    fgmask = fgbg.apply(frame)
-    medianBlur = cv2.medianBlur(fgmask, 5)
     if frame is None:
         print("视频读取完毕！")
         break
+    height, width, ret = frame.shape
+    # 视频旋转问题
+    # center = (height, width)
+    # angle = 30
+    # scale = 1
+    # rotate = cv2.getRotationMatrix2D(center, angle, scale)
 
-    # cv2.imshow("frame", frame)
-    cv2.imshow("fgbg", medianBlur)
+    # 缩小 n 倍显示视频
+    n = 3
+    small_frame = cv2.resize(frame, (width / n, height / n), interpolation=cv2.INTER_CUBIC)
 
-    k = cv2.waitKey(5) & 0xFF
+    fgmask = fgbg.apply(small_frame)
+
+    GsBlur = cv2.GaussianBlur(fgmask, (5, 5), 0)
+    # kernel = np.ones((5, 5), np.uint8)
+    # opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow("video", small_frame)
+    cv2.imshow("frame", fgmask)
+    cv2.imshow("test", GsBlur)
+
+    k = cv2.waitKey(1) & 0xFF
     if k == 27:
         print("终止播放！")
         break
