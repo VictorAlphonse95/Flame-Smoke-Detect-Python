bfa661e71294f089b14db0a4d998b1498509a87f
stone-ch <great_cy_ang@163.com>
2015-11-05 23:26:57 +0800

    modify

--- SmokeDect/test.py
+++ SmokeDect/test.py
@@ -2,67 +2,26 @@
 
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
-help_message = '''
-USAGE: peopledetect.py <image_names> ...
+trainData = np.random.randint(0, 100, (25, 2)).astype(np.float32)
+responses = np.random.randint(0, 2, (25, 1)).astype(np.float32)
 
-Press any key to continue, ESC to stop.
-'''
+red = trainData[responses.ravel() == 0]
+plt.scatter(red[:, 0], red[:, 1], 80, 'r', '^')
 
+blue = trainData[responses.ravel() == 1]
+plt.scatter(blue[:, 0], blue[:, 1], 80, 'b', 's')
 
-def inside(r, q):
-    rx, ry, rw, rh = r
-    qx, qy, qw, qh = q
-    return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
+newcomer = np.random.randint(0, 100, (1, 2))
+plt.scatter(newcomer[:, 0], newcomer[:, 1], 80, 'g', 'o')
 
+knn = cv2.ml.KNearest_create()
+knn.train(trainData, cv2.ml.ROW_SAMPLE, responses)
+ret, result, neighbours, dist = knn.findNearest(newcomer, 3)
 
-def draw_detections(img, rects, thickness=1):
-    for x, y, w, h in rects:
-        # the HOG detector returns slightly larger rectangles than the real objects.
-        # so we slightly shrink the rectangles to get a nicer output.
-        pad_w, pad_h = int(0.15 * w), int(0.05 * h)
-        cv2.rectangle(img, (x + pad_w, y + pad_h), (x + w - pad_w, y + h - pad_h), (0, 0, 255), thickness)
+print "result: ", result, "\n"
+print "neighbours: ", neighbours, "\n"
+print "distance: ", dist, "\n"
 
-
-if __name__ == '__main__':
-    import sys
-    from glob import glob
-    import itertools as it
-
-    print help_message
-
-    hog = cv2.HOGDescriptor()
-    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-    for fn in it.chain(*map(glob, sys.argv[1:])):
-        print fn, ' - ',
-        try:
-            img = cv2.imread(fn)
-            if img is None:
-                print 'Failed to load image file:', fn
-                continue
-        except:
-            print 'loading error'
-            continue
-
-        found, w = hog.detectMultiScale(img, winStride=(8, 8), padding=(32, 32), scale=1.05)
-        print "found:",
-        print found
-        print "w:",
-        print w
-        found_filtered = []
-        for ri, r in enumerate(found):
-            for qi, q in enumerate(found):
-                if ri != qi and inside(r, q):
-                    break
-            else:
-                found_filtered.append(r)
-        draw_detections(img, found)
-        draw_detections(img, found_filtered, 3)
-        print '%d (%d) found' % (len(found_filtered), len(found))
-        cv2.imshow('img', img)
-        # cv2.imwrite('../images/hog.png', img)
-        ch = 0xFF & cv2.waitKey()
-        if ch == 27:
-            break
-    cv2.destroyAllWindows()
+plt.show()
