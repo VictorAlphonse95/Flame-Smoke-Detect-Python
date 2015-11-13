c50f2729eb1bd24026a8ca61efb81e7023b0a09a
stone-ch <great_cy_ang@163.com>
2015-11-13 21:35:45 +0800

    modify

--- FlameDect/flameDection.py
+++ FlameDect/flameDection.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('../fire/fd.png', 0)
+img = cv2.imread('fire/fd.png', 1)
 #img = cv2.medianBlur(img,5)
 img2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C,\
     cv2.THRESH_BINARY, 11, 2)
